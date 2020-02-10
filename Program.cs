baf49b00c164066590cabc6cc472459770c162a4
1321200 <55232721+A2JA2@users.noreply.github.com>
2020-02-10 10:22:25 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2D_array_examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[,] array = new string[,] { { "Ubisoft", "Valve Corporation" }, { "Nintendo", "Sega" }, };
+            // ... Print out values.
+            Console.WriteLine(array[0, 0]);
+            Console.WriteLine(array[0, 1]);
+            Console.WriteLine(array[1, 0]);
+            Console.WriteLine(array[1, 1]);
+            Console.ReadKey();
+        }
+    }
+}
